ea1ac511321e81e6ef6076b8ddbfed2c7972db0a
kdm1jkm <kdm1jkmm@gmail.com>
2021-01-18 15:14:04 +0900

    Add comment.

--- main.cpp
+++ main.cpp
@@ -4,18 +4,28 @@
 
 
 int main() {
+    // Files
     const std::string BALL_TEXTURE_DIRECTORY("resources/example_ball-Sheet.png");
+    const std::string FONT_DIRECTORY("resources/NanumGothic.ttf");
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Tutorial");
+    // New window
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Tutorial");
 
+    // For animating
     sf::Time time = sf::milliseconds(0);
     int sheet = 0;
     const sf::Time interval = sf::milliseconds(100);
     const int sheets = 2;
 
+    // Load texture
     sf::Texture ballTexture;
-    ballTexture.loadFromFile(BALL_TEXTURE_DIRECTORY, sf::Rect<int>(0, 0, 16, 8));
+    if (!(ballTexture.loadFromFile(BALL_TEXTURE_DIRECTORY, sf::Rect<int>(0, 0, 16, 8)))) {
+        return EXIT_FAILURE;
+    }
+    // Anti-aliasing
+//    ballTexture.setSmooth(true);
 
+    // Make sprite
     sf::Sprite ballSprite(ballTexture, sf::IntRect(8, 0, 8, 8));
     ballSprite.setPosition(sf::Vector2f(100, 100));
     ballSprite.setScale(sf::Vector2f(5, 5));
@@ -25,6 +35,7 @@ int main() {
 //    window.setVerticalSyncEnabled(true);
     window.setFramerateLimit(120);
 
+    // Main loop
     sf::Event event; // NOLINT(cppcoreguidelines-pro-type-member-init)
     sf::Clock clock;
     while (window.isOpen()) {
@@ -40,6 +51,7 @@ int main() {
         }
         window.clear(sf::Color::Green);
 
+        // Animate
         if (time > interval) {
             ballSprite.setTextureRect(sf::IntRect(8 * sheet, 0, 8, 8));
 
@@ -48,9 +60,9 @@ int main() {
             time = sf::milliseconds(0);
         }
 
+        // Draw things
         window.draw(ballSprite);
 
-        // Handle event
         sf::Time elapsed = clock.restart();
         time += elapsed;
 //        std::cout << elapsed.asSeconds() << std::endl;
