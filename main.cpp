ceef74b1e5048948cbd4347673d3500cec4a633a
kdm1jkm <kdm1jkmm@gmail.com>
2021-01-18 10:18:58 +0900

    Set frameRate.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
 
 int main() {
     Window window(VideoMode(800, 600), "Tutorial");
+
+    // NEVER USE BOTH.
+    // run at the same frequency as the monitor's refresh rate.
+//    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(120);
 
     Event event; // NOLINT(cppcoreguidelines-pro-type-member-init)
     while (window.isOpen()) {
